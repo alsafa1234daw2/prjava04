20e4c8003dc5f22c33b22f58e94ac60953bb2e20
Alex <Alex@192.168.0.196>
2014-05-03 10:15:05 +0200

    branca master

---
+++ src/prjava04/Prjava04.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prjava04;
+import java.io.*; import java.net.*;
+
+/**
+ *
+ * @author Alex
+ */
+public class Prjava04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { try {
+        InetAddress addr = InetAddress.getLocalHost(); byte[] ipAddr = addr.getAddress();
+        String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
+        System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+        }
+        catch (IOException e) { e.printStackTrace();
+    }}
+}
